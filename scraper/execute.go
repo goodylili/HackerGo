2da3cafacb1249500b1a314e81878a6e8c4c30de
Goodnessuc <ukejegoodness599@gmail.com>
2022-06-30 00:05:58 +0100

    Changes

---
+++ scraper/execute.go
@@ -0,0 +1,57 @@
+package scraper
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+/*
+RandomNewsPage scrapes a random news page using the ScrapeNews function
+*/
+
+/*
+AllFromPage prints all the news in a page8
+*/
+func AllFromPage(page int) {
+	scrapePage := ScrapeNews(page)
+	for _, structs := range scrapePage {
+		format := fmt.Sprintf("%s \n %s \n\n", structs.Title, structs.Link)
+		fmt.Println(format)
+	}
+}
+
+/*
+RandomNewsPage
+*/
+func RandomNewsPage(page int) int {
+	rand.Seed(time.Now().UnixNano())
+	random := rand.Intn(30-1) + 1
+	return random
+}
+
+// NumberOfJobs selects news to be displayed using the random or a specified page, It just controls the number of news to be displayed/*
+func NumberOfJobs(number int) {
+	jobs := ScrapeJobs()
+	for index, structs := range jobs {
+		if index <= number {
+			format := fmt.Sprintf("%s \n %s \n\n", structs.Title, structs.Link)
+			fmt.Println(format)
+		}
+
+	}
+
+}
+
+// NumberOfNews selects news to be displayed using the random or a specified page, It just controls the number of news to be displayed/*
+func NumberOfNews(pages, number int) {
+	jobs := ScrapeNews(pages)
+	for index, structs := range jobs {
+		if index <= number {
+			format := fmt.Sprintf("%s \n %s \n\n", structs.Title, structs.Link)
+			fmt.Println(format)
+		}
+
+	}
+
+}
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -3,15 +3,12 @@ package scraper
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	"math/rand"
 	"net/http"
-	"time"
 )
 
 /*
 ScrapeNews Scrapes the website for other handlers to use based on the specified page
 */
-
 func ScrapeNews(page int) []Information {
 	var Info = make([]Information, 0)
 	webUrl, num := "https://news.ycombinator.com/news?p=", page
@@ -33,41 +30,6 @@ func ScrapeNews(page int) []Information {
 
 	})
 	return Info
-}
-
-/*
-RandomNewsPage scrapes a random news page using the ScrapeNews function
-*/
-func RandomNewsPage(page int) []Information {
-	rand.Seed(time.Now().UnixNano())
-	random := rand.Intn(30-1) + 1
-	return ScrapeNews(random)
-}
-
-// NumberOfJobs selects news to be displayed using the random or a specified page, It just controls the number of news to be displayed/*
-func NumberOfJobs(number int) {
-	jobs := ScrapeJobs()
-	for index, structs := range jobs {
-		if index <= number {
-			format := fmt.Sprintf("%s \n %s \n\n", structs.Title, structs.Link)
-			fmt.Println(format)
-		}
-
-	}
-
-}
-
-// NumberOfNews selects news to be displayed using the random or a specified page, It just controls the number of news to be displayed/*
-func NumberOfNews(pages, number int) {
-	jobs := ScrapeNews(pages)
-	for index, structs := range jobs {
-		if index <= number {
-			format := fmt.Sprintf("%s \n %s \n\n", structs.Title, structs.Link)
-			fmt.Println(format)
-		}
-
-	}
-
 }
 
 func selectRandom() {
@@ -93,7 +55,7 @@ func ScrapeJobs() []Information {
 
 	})
 
-	// 31642927
+	//
 
 	return Info
 }
